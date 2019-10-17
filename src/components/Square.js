5e79093fb56a44cb3f9017c234d58be29c5d2d6c
Dacey Nolan <daceyshirley@gmail.com>
2019-10-17 16:29:23 -0500

    Refactor to a class component

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,16 +1,18 @@
 import React from "react";
 
-const Square = props => {
-  return (
-    <button
-      className="square"
-      onClick={() => {
-        alert("click");
-      }}
-    >
-      {props.value}
-    </button>
-  );
-};
+class Square extends React.Component {
+  render() {
+    return (
+      <button
+        className="square"
+        onClick={() => {
+          alert("click");
+        }}
+      >
+        {this.props.value}
+      </button>
+    );
+  }
+}
 
 export default Square;
