28bb9ff40c64bcd13153aa99e5dac657440c7b65
Dacey Nolan <daceyshirley@gmail.com>
2019-10-18 14:18:04 -0500

    Create function to keep track of move history

--- src/components/Game.js
+++ src/components/Game.js
@@ -38,6 +38,15 @@ class Game extends React.Component {
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
 
+    const moves = history.map((step, move) => {
+      const desc = move ? "Go to move #" + move : "Go to game start";
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
     let status;
     if (winner) {
       status = "Winner: " + winner;
@@ -55,7 +64,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
